0284e37485b94117690a5f4f14a196d18834a2de
Connor Burton <connorjburton@gmail.com>
2015-11-25 17:08:03 +0000

    Adding Event tracking job

---
+++ app/Jobs/StoreEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Http\Controllers\Event\EventTrait;
+
+class StoreEvent extends Job implements SelfHandling, ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels, EventTrait;
+
+    protected $id;
+    protected $options;
+    protected $callback;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($id, $options, $callback)
+    {
+        $this->id = $id;
+        $this->options = $options;
+        $this->callback = $callback;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->storeEvent($this->id, $this->options, $this->callback);
+    }
+}
